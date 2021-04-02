d8d8034a315453f003a109f1c9254efb3b29851f
jbeedle19 <josh.beedle@gmail.com>
2021-04-02 11:16:12 -0400

    updated app.js with redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { StoreProvider } from './utils/GlobalState';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -30,7 +31,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -41,7 +42,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
